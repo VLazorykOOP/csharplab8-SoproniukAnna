85fc0959aeb320a76ff79b18143f3f8d7a9b9838
SoproniukAnna <124136300+SoproniukAnna@users.noreply.github.com>
2024-04-17 20:51:15 +0300

    Final*

--- Lab8CSharp/Program.cs
+++ Lab8CSharp/Program.cs
@@ -8,14 +8,17 @@ internal class Program
         //Task1 task1 = new Task1("Task1.txt", "Task1Res.txt");
         //task1.ProcessText();
 
+
         //Console.WriteLine("Task2");
         //int wordLength = 3 ; 
         //Task2 task2 = new Task2("Task2.txt", "Task2Res.txt");
         //task2.FilterWordsByLength(wordLength);
 
+
         //Console.WriteLine("Task3");
         //Task3 task3 = new Task3("Task3.txt", "Task3Res.txt");
         //task3.RemoveWordsWithOddLength();
+
 
         /*Console.WriteLine("Task4");
         string[] words = { "apple", "banana", "orange", "grape", "kiwi", "melon", "peach" };
@@ -34,14 +37,13 @@ internal class Program
 
 
         Console.WriteLine("Task5");
-        // Крок 1: Створення папок
         string folder1Path = @"d:\temp1\Soproniuk_Anna1";
         string folder2Path = @"d:\temp1\Soproniuk_Anna2";
 
         Directory.CreateDirectory(folder1Path);
         Directory.CreateDirectory(folder2Path);
 
-        // Крок 2: Створення файлів та запис тексту
+
         string t1Path = Path.Combine(folder1Path, "t1.txt");
         string t2Path = Path.Combine(folder1Path, "t2.txt");
 
@@ -55,7 +57,8 @@ internal class Program
             sw.WriteLine("<Комар Сергій Федорович, 2000 > року народження, місце проживання <м. Київ>");
         }
 
-        // Крок 3: Зчитування та запис тексту з файлів t1.txt та t2.txt у файл t3.txt
+
+
         string t3Path = Path.Combine(folder2Path, "t3.txt");
 
         File.Copy(t1Path, t3Path, true);
@@ -66,33 +69,36 @@ internal class Program
             sw.WriteLine(File.ReadAllText(t2Path));
         }
 
-        // Крок 4: Виведення інформації про створені файли
-        Console.WriteLine($"Створені файли:");
-        Console.WriteLine($"t1.txt: {new FileInfo(t1Path).Length} байт");
-        Console.WriteLine($"t2.txt: {new FileInfo(t2Path).Length} байт");
-        Console.WriteLine($"t3.txt: {new FileInfo(t3Path).Length} байт");
 
-        // Крок 5: Переміщення файлу t2.txt у папку Soproniuk_Anna2
+
+        Console.WriteLine($"Created files::");
+        Console.WriteLine($"t1.txt: {new FileInfo(t1Path).Length} byte");
+        Console.WriteLine($"t2.txt: {new FileInfo(t2Path).Length} byte");
+        Console.WriteLine($"t3.txt: {new FileInfo(t3Path).Length} byte");
+
+
+
         string t2NewPath = Path.Combine(folder2Path, "t2.txt");
         File.Move(t2Path, t2NewPath);
 
-        // Крок 6: Копіювання файлу t1.txt у папку Soproniuk_Anna2
+
+
         string t1NewPath = Path.Combine(folder2Path, "t1.txt");
         File.Copy(t1Path, t1NewPath);
 
-        // Крок 7: Перейменування папки K2 в ALL та видалення папки Soproniuk_Anna1
+
+
         string folderAllPath = @"d:\temp1\ALL";
         Directory.Move(folder2Path, folderAllPath);
         Directory.Delete(folder1Path, true);
-        //Directory.Delete(folder1Path);
 
 
-        // Крок 8: Виведення повної інформації про файли папки All
-        Console.WriteLine($"Файли в папці ALL:");
+
+        Console.WriteLine($"Files in the 'ALL' folder:");
         string[] allFiles = Directory.GetFiles(folderAllPath);
         foreach (string file in allFiles)
         {
-            Console.WriteLine($"{Path.GetFileName(file)}: {new FileInfo(file).Length} байт");
+            Console.WriteLine($"{Path.GetFileName(file)}: {new FileInfo(file).Length} byte");
         }
 
         Console.ReadKey();
